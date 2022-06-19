72ebbe86c4cec5be76fb87a73b3404c49763838e
strugglinglee <strugglinglee@163.com>
2022-06-19 22:28:32 +0800

    feat: add api

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -8,11 +8,19 @@
 import { ref, onMounted } from 'vue'
 import { useSystem } from '@/stores'
 import './index.scss'
+import request from '@/utils/request'
 
 const msg = ref('Hello world .')
 
 onMounted(() => {
   const sys = useSystem()
   console.log(sys.options, 'sys')
+  request({
+    type: 'get',
+    url: '/dj/program',
+    params: {
+      rid: 336355127,
+    },
+  })
 })
 </script>
