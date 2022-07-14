1f193c26db10df30b95a4e5451dc91383c00d11a
strugglinglee <strugglinglee@163.com>
2022-07-14 16:58:07 +0800

    feat: 简陋版本歌词详情页

---
+++ src/components/header.vue
@@ -0,0 +1,37 @@
+<template>
+  <view class="header">
+    <view class="icon" @click="back">&lt;</view>
+    <view class="title">
+      <slot></slot>
+    </view>
+  </view>
+</template>
+
+<script lang="ts" setup>
+import { ref, onMounted } from 'vue'
+import Taro from '@tarojs/taro'
+// const emit = defineEmits(['search'])
+
+onMounted(async () => {})
+const back = () => {
+  Taro.navigateBack()
+}
+</script>
+
+<style lang="scss">
+.header {
+  display: flex;
+  align-items: flex-end;
+  height: 150rpx;
+  padding: 0 20rpx;
+
+  .icon {
+    width: 50rpx;
+    height: 50rpx;
+    font-size: 30rpx;
+  }
+
+  .title {
+  }
+}
+</style>
