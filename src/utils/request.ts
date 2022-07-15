c7aab156e8348273b44d41537b131db23deab2c9
strugglinglee <strugglinglee@163.com>
2022-07-15 18:26:22 +0800

    feat: ts有点难= =

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -21,7 +21,7 @@ instance.interceptors.request.use((config: AxiosRequestConfig) => {
   return { ...config, baseURL }
 })
 
-const showMessage = (title: unknown) => {
+const showMessage = (title: string) => {
   const message = JSON.stringify(title).replace(/"/g, '')
   // TODO Request failed with status code 500 优化展示逻辑
   if (message.indexOf('Network') > -1) {
@@ -34,12 +34,11 @@ const showMessage = (title: unknown) => {
 }
 interface ApiResult<T> {
   code: number
-  message?: string
-  result: T
+  status?: number
+  data: T
 }
 // Taro.showToast 和loading 是单例 所以只有成功时候hideLoading 其他情况showToast
 export default function request<T>(options: AxiosRequestConfig = {}) {
-  // console.log(apiBaseUrl, options)
   return new Promise<T>((resolve, reject) => {
     instance(options)
       .then((response: AxiosResponse<ApiResult<T>>) => {
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -7,3 +7,35 @@ export const showToast = (title: string) => {
     duration: 3000,
   })
 }
+
+// 防抖函数
+export const debounce = function(fn, delay = 100) {
+  type Timeout = ReturnType<typeof setTimeout> | null
+  let timer: Timeout = null
+  return function() {
+    const context = this
+    // eslint-disable-next-line prefer-rest-params
+    const args: IArguments = arguments
+    if (timer) {
+      clearTimeout(timer)
+    }
+    timer = setTimeout(function() {
+      fn.apply(context, args)
+    }, delay)
+  }
+}
+
+// 节流函数
+export const throttle = function(fn, interval = 100) {
+  let last = 0
+  return function() {
+    const context = this
+    // eslint-disable-next-line prefer-rest-params
+    const args: IArguments = arguments
+    const now = +new Date()
+    if (now - last >= interval) {
+      last = now
+      fn.apply(context, args)
+    }
+  }
+}
