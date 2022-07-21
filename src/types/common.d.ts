80a7c2a57b93222dd9a81dee93bcc1f57a0696ac
strugglinglee <strugglinglee@163.com>
2022-07-21 19:24:53 +0800

    feat: 小change

--- src/types/common.d.ts
+++ src/types/common.d.ts
@@ -1,6 +1,8 @@
 // https://developers.weixin.qq.com/miniprogram/dev/reference/configuration/page.html
 declare const wx: object
 
-interface SongDetail {
+export interface UnknownRes {
   [propName: string]: unknown
 }
+
+export interface SongDetail extends UnknownRes {}
